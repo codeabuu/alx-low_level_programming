f76c0e096890d2fc31a7fb1e12dfb22cc95b8ea0
codeabuu <abdulngui718@gmail.com>
2023-03-16 06:23:01 -0700

    size char

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - prints size of various comp types
+ * Return 0 (success)
+ */
 int main(void)
 {
 char c;
@@ -7,10 +10,10 @@ int i;
 float r;
 long int y;
 long long int z;
-printf("Size of a char is: %lu.\n", (unsigned long)sizeof(c));
-printf("Size of int is: %lu.\n", (unsigned long)sizeof(i));
-printf("Size of a float is: %lu.\n", (unsigned long)sizeof(r));
-printf("Size of a long int is: %lu.\n", (unsigned long)sizeof(y));
-printf("Size of long long int is: %lu.\n", (unsigned long)sizeof(z));
+printf("Size of a char is: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of int is: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a float is: %lu byte(s)\n", (unsigned long)sizeof(r));
+printf("Size of a long int is: %lu byte(s)\n", (unsigned long)sizeof(y));
+printf("Size of long long int is: %lu byte(s)\n", (unsigned long)sizeof(z));
 return (0);
 }
