e2260835f96ac5c85e6a659c177d98699994a765
codeabuu <abdulngui718@gmail.com>
2023-03-16 12:53:10 -0700

    chars

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,5 +15,6 @@ printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(g));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+fprintf(stderr, "[Anything]\n");
 return (0);
 }
