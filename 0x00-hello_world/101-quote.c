dabba008350876be7f8602856742b204b287e67a
codeabuu <abdulngui718@gmail.com>
2023-03-16 13:38:05 -0700

    NIX is basically a simple OS

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+#include <unistd.h>
+/**
+ * main - A c program that prints a line to the standard error
+ * Return 1 (Success)
+ */
 int main(void)
 {
-echo ("and that piece of art is useful" - Dora Korpar, 2015-10-19)
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
