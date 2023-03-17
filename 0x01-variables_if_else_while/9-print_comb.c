6936c152059b1c886d959bba38e31cc65fd8e9d2
codeabuu <abdulngui718@gmail.com>
2023-03-17 06:59:45 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+
 /**
- * main - combine nos
- *
+ * main - prints all possible combinations of single-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -17,6 +17,6 @@ int main(void)
 			putchar(' ');
 		}
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
