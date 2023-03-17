d2b996020a752a0f42e5541720c96d19f15de0f6
codeabuu <abdulngui718@gmail.com>
2023-03-17 06:25:59 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int z;
+
+	for (z = 48; z < 58; z++)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}
