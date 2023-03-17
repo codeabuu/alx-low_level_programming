f6ab2d2fab4700590932e0f2cb3ba583267b955d
codeabuu <abdulngui718@gmail.com>
2023-03-17 06:08:21 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - single digit nos
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0;a < 9;a++)
+	{
+		printf("%d\n", a);
+	}
+	return (0);
+}
