2b7668363f382b3f49c0e46f8776ca91d9467ea5
codeabuu <abdulngui718@gmail.com>
2023-03-17 05:56:56 -0700

    When I was having that alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 /**
- * main - lowerscase except q,e.
+ *main - Prints the alphabet without q and e.
  *
- * Return: Always 0 (Succesful)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char alp[24] = "abcdfghijklmnoprstuvwxyz";
-	int j;
+	int i;
 
-	for (j = 0; j < 24; j++)
+	for (i = 97; i < 123; i++)
 	{
-		putchar(alp[j]);
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
