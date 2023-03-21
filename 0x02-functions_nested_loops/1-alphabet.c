cce820942299f1e351ac0eeb1d146fd16272f74c
codeabuu <abdulngui718@gmail.com>
2023-03-21 05:20:11 -0700

    alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
-/**
- * print_alphabet - print all alphabet in lowercase
- *
- * Return: Always 0 (Success)
- */
-void print_alphabet(void)
-{
-	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-		_putchar("\n);
-}
+	/**
+	 * print_alphabet - print all alphabet in lowercase
+	 */
+
+
+	void print_alphabet(void)
+	{
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
+	}
