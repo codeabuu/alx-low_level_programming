9c86de1c56d415ae8c9e181e8396b1575cb87a75
codeabuu <abdulngui718@gmail.com>
2023-03-21 05:07:16 -0700

    I play what I call the alphabet game

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 	/**
-	 * main - print _putchar is written by mbah
-	 *
+	 * main(void) - print _putchar
+	 * is written by mbah
 	 * Return: 0
 	 */
 
 
-	int main(void)
+	int main(void)/* main -entry point*/
 	{
 	_putchar('_');
 	_putchar('p');
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+		_putchar("\n);
+}
