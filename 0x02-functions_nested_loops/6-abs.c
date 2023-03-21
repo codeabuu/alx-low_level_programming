9fe40e9d554d73eef3b33c487cd7eb51f81d939d
codeabuu <abdulngui718@gmail.com>
2023-03-21 09:22:53 -0700

    ou can only estimate what a thing is worth to you

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - absolute value of an integer
+ *
+ * @n: the int to be checked
+ * int - integer
+ *
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+return (-n);
+}
+
