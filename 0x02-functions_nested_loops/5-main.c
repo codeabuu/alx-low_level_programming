c010ab75610ad9098f33a4d5d797cda6cd2a5173
codeabuu <abdulngui718@gmail.com>
2023-03-21 08:58:43 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * main - check code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{
+		int r;
+
+r = print_sign(98);
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+r = print_sign(0);
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+putchar('\n');
+r = print_sign(0xff);
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+r = print_sign(-1);
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+return (0);
+	}
