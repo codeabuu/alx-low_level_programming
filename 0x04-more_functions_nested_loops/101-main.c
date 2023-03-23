aadfff2bd0ce0fb985760e274790d553ff06f2e2
codeabuu <abdulngui718@gmail.com>
2023-03-23 05:58:00 -0700

    Numbers have life

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -8,14 +8,14 @@
 int main(void)
 {
 	print_number(98);
-	_putchar('\n');
+	putchar('\n');
 	print_number(402);
-	_putchar('\n');
+	putchar('\n');
 	print_number(1024);
-	_putchar('\n');
+	putchar('\n');
 	print_number(0);
-	_putchar('\n');
+	putchar('\n');
 	print_number(-98);
-	_putchar('\n');
+	putchar('\n');
 	return (0);
 }
