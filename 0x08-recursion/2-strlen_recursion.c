28d186d3ebfed2cfea93123bcfbf47c48b70b7c2
codeabuu <abdulngui718@gmail.com>
2023-04-05 04:39:57 -0700

    Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -7,3 +7,7 @@ Prototype: void _puts_recursion(char *s)
 Write a function that prints a string in reverse.
 
 Prototype: void _print_rev_recursion(char *s);
+
+Write a function that returns the length of a string.
+
+Prototype: int _strlen_recursion(char *s);
