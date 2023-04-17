f20eda946733f4f728b6a410c16813500d39b3a8
codeabuu <abdulngui718@gmail.com>
2023-04-17 08:09:17 -0700

    File Name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ * _FILE_ - file name for this code
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
