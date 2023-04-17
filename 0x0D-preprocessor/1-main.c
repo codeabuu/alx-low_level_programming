3c90330f0a970a4f08633e8583c1ed7e544fc32c
codeabuu <abdulngui718@gmail.com>
2023-04-17 08:06:28 -0700

    Pi

---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,19 @@
+#include "1-pi.h"
+#include "1-pi.h"
+#include <stdio.h>
+
+/**
+ * main - checks the whole code
+ *
+ * Return: is always 0.
+ */
+int main(void)
+{
+	float a;
+	float r;
+
+	r = 98;
+	a = PI * r * r;
+	printf("%.3f\n", a);
+	return (0);
+}
