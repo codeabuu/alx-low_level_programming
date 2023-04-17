d99dde232c19f640002f6dd5041c9189af107366
codeabuu <abdulngui718@gmail.com>
2023-04-17 07:56:28 -0700

    Object-like Macro

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,17 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - will check the code
+ *
+ * Return: is Always 0.
+ */
+int main(void)
+{
+	int s;
+
+	s = 98 + SIZE;
+	printf("%d\n", s);
+	return (0);
+}
