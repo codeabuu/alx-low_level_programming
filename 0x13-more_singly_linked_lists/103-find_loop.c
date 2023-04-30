e0bab013c3e7b8d464f889ca56190378b676da69
codeabuu <abdulngui718@gmail.com>
2023-04-30 07:18:53 -0700

    14. Find the loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,36 +1,27 @@
-#include "lists.h"
-
-/**
-* free_listint_safe - frees a linked list
-* @h: pointer to the first node in the linked list
-*
-* Return: number of elements in the freed list
-*/
-size_t free_listint_safe(listint_t **h)
+listint_t *find_listint_loop(listint_t *head)
 {
-size_t len = 0;
-int diff;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
-if (!h || !*h)
-return (0);
+	if (!head)
+		return (NULL);
 
-for (; *h; len++)
-{
-diff = *h - (*h)->next;
-if (diff > 0)
-{
-listint_t *temp = (*h)->next;
-free(*h);
-*h = temp;
-}
-else
-{
-free(*h);
-*h = NULL;
-break;
-}
-}
+	for (; slow && fast && fast->next;)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+		{
+			slow = head;
+			for (; slow != fast;)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
+		}
+	}
 
-return (len);
+	return (NULL);
 }
 
