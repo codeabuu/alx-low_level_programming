11d6cbd82b579b215f5676944b3c326faf17b55b
codeabuu <abdulngui718@gmail.com>
2023-05-08 02:31:33 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -93,8 +93,7 @@ int main(int argc, char *argv[])
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
 
-	} 
-	while (r > 0);
+	} while (r > 0);
 
 	free(buffer);
 	close_file(from);
