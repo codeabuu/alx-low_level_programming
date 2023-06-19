99672f540fcec2348e23651b0a675ac1d85f491d
codeabuu <abdulngui718@gmail.com>
2023-06-19 05:36:01 -0700

    c func in py

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,17 +1,24 @@
-#include <stdio.h>
 int add(int a, int b)
 {
-	return(a + b);
+  return (a + b);
 }
-int mul(int a, int b)
+
+int sub(int a, int b)
 {
-	return(a * b);
+  return (a - b);
 }
+
 int div(int a, int b)
 {
-	return(a / b);
+  return (a / b);
 }
+
+int mul(int a, int b)
+{
+  return (a * b);
+}
+
 int mod(int a, int b)
 {
-	return(a % b);
+  return (a % b);
 }
